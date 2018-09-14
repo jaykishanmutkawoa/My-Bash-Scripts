0130071f6ce3c11bfd8ab70be22a5d0d5fe627ce
TheTunnelix <jmutkawoa@gmail.com>
2018-09-14 01:55:35 -0400

    adding curl external response time

---
+++ CurlExternalResponseTime.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author: jmutkawoa@gmail.com
+
+URL="https://tunnelix.com"
+
+RESULT=`curl -o /dev/null -s -w "Time:%{time_total} Status:%{http_code}" $URL`
+
+date=`date`
+
+echo "$RESULT-$date" >> /home/output/result_external.txt
